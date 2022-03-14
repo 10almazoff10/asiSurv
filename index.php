859babb53a424608d30e95685a074bd50821a79e
Oleg Prokin <48990114+10almazoff10@users.noreply.github.com>
2022-03-14 09:12:48 +0700

    update 14/03

--- index.php
+++ index.php
@@ -23,7 +23,9 @@ include "php/Workers.php";
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="/js/HideSeek-master/jquery.hideseek.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
+    <link rel="icon" class="fav" href="img/favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -45,14 +47,25 @@ include "php/Workers.php";
     <div class="left_panel box3d">
         <div class="workers_list">
             <div class="navbar">
-                <input class="form-control" id="search" name="search" type="text" data-toggle="hideseek" data-list=".list-group">
+                <input placeholder="Начните писать фамилию..." class="form-control" id="search" name="search" type="text" data-toggle="hideseek" data-list=".form_radio_btn" autofocus>
             </div>
+
+            <form method="POST" id="ajax_form" action="" >
+
             <ul class="list-group" >
-                
+
+                <div class="form_radio_btn">
+
                     <?php
-                        $a= new Workers();
-                        $a->list_workers();
+                    $a= new Workers();
+                    $a->list_workers();
                     ?>
+
+                </div>
+                
+
+
+
                 
                 
             </ul>
@@ -61,11 +74,11 @@ include "php/Workers.php";
     <div class="mid_pannel box3d">
         <div class="top_picker box3d">
 
-            <form method="post" id="ajax_form" action="" >
+
 
 
                 <div class='input-group date' id='datetimepicker10'>
-                    <input name="date" type='text' class="form-control" />
+                    <input placeholder="Выберите дату" name="date" type='text' class="form-control" />
                     <span class="input-group-addon">
                         <span class="glyphicon glyphicon-calendar">
                         </span>
@@ -88,10 +101,9 @@ include "php/Workers.php";
 
 
             <div class="worker_table box3d" id="result_form"></div>
-
-
-
-</div>
+                
+            
+            </div>
 
 
 
@@ -101,10 +113,10 @@ include "php/Workers.php";
             locale: 'ru',
             viewMode: 'years',
             format: 'MM/YYYY'
+
         });
     });
 </script>
-
 
 
 
